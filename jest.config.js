f0cb77bb7a2d41d9b0ae07d4be413faf678fc679
mkwtys <mkwtys@gmail.com>
2019-01-27 14:52:59 +0900

    fix typescript in jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.(ts|js)'],
   coveragePathIgnorePatterns: ['/node_modules/', 'fixtures'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  transformIgnorePatterns: ['/node_modules/'],
-  testMatch: [`src/**/__tests__/*.+(ts|js)`, `**/?(*.)+(spec|test).(ts|js)?(x)`]
+  testPathIgnorePatterns: ['/node_modules/', 'fixtures']
 }
